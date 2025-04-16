09a17ca16bd30de89f32a55502f5e1e7bffdae78
lea287 <deepseanight@gmail.com>
2025-04-16 13:13:06 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -10,6 +10,13 @@ def webhook():
         f.write(data)
     return "Signal reçu", 200
 
+@app.route('/show-signal', methods=['GET'])
+def show_signal():
+    if os.path.exists("signal.txt"):
+        with open("signal.txt", "r") as f:
+            return f.read(), 200
+    return "Aucun signal reçu", 200
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
